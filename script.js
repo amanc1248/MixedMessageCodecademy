3e3d4f8ecf5f66b1c72f973625c423ca6336090c
amanc1248 <amanc1248@gmail.com>
2022-03-08 13:11:38 +0545

    ADD: 4 array of mesages

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Lets go");
+console.log("<======THIS IS YOUR FUTURE JOB DETECTOR=====>");
+
+// creating random message array
+const jobMonths = [1, 2, 3, 4, 5];
+const jobCompany = ["Method Financial", "Evidence", "Lugg", "Informed K2"];
+const jobRole = [
+  "Full Stack Engineer",
+  "Front-end Engineer",
+  "Back-end Engineer",
+  "Data Analyst",
+];
+const jobSalary = [100, 105, 70, 150, 70];
