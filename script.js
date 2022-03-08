13d3cf940102d23465f41bb3f5dcd12873673e21
amanc1248 <amanc1248@gmail.com>
2022-03-08 13:18:32 +0545

    ADD: random functions

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ const jobSalary = [100, 105, 70, 150, 70];
 
 // final output message
 let finalMessage = "";
+
+// creating random messages
+const randJobMonths = (jobMonths) => {
+  let num = Math.floor(Math.random() * jobMonths.length);
+  finalMessage =
+    finalMessage +
+    `Hi! Aman you will get a job after ${jobCompany[num]} month `;
+};
+const randJobCompany = (jobCompany) => {
+  let num = Math.floor(Math.random() * jobCompany.length);
+  finalMessage = finalMessage + `at ${jobCompany[num]} company `;
+};
+const randJobRole = (jobRole) => {
+  let num = Math.floor(Math.random() * jobRole.length);
+  finalMessage = finalMessage + `as a ${jobRole[num]} `;
+};
+const randJobSalary = (jobSalary) => {
+  let num = Math.floor(Math.random() * jobSalary.length);
+  finalMessage = finalMessage + `at ${jobSalary[num]}k/year.`;
+};
